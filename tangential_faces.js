592ae33280d23840ceea258b9b19363b284a9269
Hermann-SW <hermann@stamm-wilbrandt.de>
2024-06-19 18:14:27 +0200

    Display normals of tangential faces

--- tangential_faces.js
+++ tangential_faces.js
@@ -1,8 +1,8 @@
 const jscad = require('@jscad/modeling')
 const { geom3 } = jscad.geometries
 const { colorize } = jscad.colors
-const { sphere } = jscad.primitives
-const { translate } = jscad.transforms
+const { sphere, cylinder } = jscad.primitives
+const { translate, rotate, scale:scale3d } = jscad.transforms
 const { vec3 } = jscad.maths
 
 const det = ([[a,b,c],[d,e,f],[g,h,i]]) => a*(e*i-f*h)-b*(d*i-f*g)+c*(d*h-e*g)
@@ -14,8 +14,24 @@ const solve = (A,b) =>
 const assert = (b) => {if(!b){throw("assert")}}
 
 segments = 6
+const oneCylinder = cylinder({radius: 1, height: 1, segments: segments})
 let reusedsphere = sphere({radius:1, segments: segments})
 function fastvertex(c) { return(translate(c, reusedsphere)) }
+
+function edge(_v, _w, r = 0.05, segs = segments) {
+    d = [0, 0, 0]
+    w = [0, 0, 0]
+    vec3.subtract(d, _w, _v)
+    vec3.add(w, _v, _w)
+    vec3.scale(w, w, 0.5)
+    return translate(w,
+             rotate([0, Math.acos(d[2]/vec3.length(d)), Math.atan2(d[1], d[0])],
+               scale3d([r,r,vec3.length(d)],oneCylinder)
+             )
+           )
+}
+
+const line3 = ((_v, _w) => edge(_v, _w, 0.05, segments));
 
 const tol = 1e-6
 const equals = (v,w) => Math.abs(v-w)<tol
@@ -69,7 +85,8 @@ function main(params) {
 
   return [colorize([0,0,1,0.5],h), u.map((v)=>fastvertex(v)), 
           colorize([0.5,0.5,0.5,0.6], g),
-          colorize([1,1,1], fastvertex(c))]
+          colorize([1,1,1], fastvertex(c)),
+          u.map((p)=> colorize([1,1,1], line3(c,p)))]
 }
 
 function getParameterDefinitions() {
